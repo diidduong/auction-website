f7832e16dabd2fbe7957cc758e83e259637712c0
Duy Duong <diidduong@gmail.com>
2022-12-10 18:44:02 -0700

    add ranking functionalities

--- flaskr/ranking.py
+++ flaskr/ranking.py
@@ -21,34 +21,61 @@ bp = Blueprint("ranking", __name__)
 @bp.route("/ranking")
 def index():
     """Show all the posts, most recent first."""
+    types = [
+        {"name":"Top Ask Price", "path":"/ranking/top-ask-price"}
+    ]
 
-    redirect(url_for("ranking.get_top_ask_price"))
+    return redirect(url_for("ranking.get_top_ask_price"))
 
 # Highest number of posts, frequent seller
 @bp.route("/ranking/top-freq-sellers")
 def get_top_freq_sellers():
-    return render_template("ranking/ranking.html")
+    db = get_db()
+    cursor = db.cursor()
+    users = cursor.execute(
+        "SELECT u.id, u.firstname, u.lastname, p.author_id, COUNT(*)"
+        " FROM user u JOIN post p ON u.id = p.author_id"
+        " GROUP BY u.id"
+        " ORDER BY COUNT(p.author_id) DESC"
+    )
+
+    return render_template("ranking/ranking.html", users=users)
 
 # Highest number of posts, frequent bidder
 @bp.route("/ranking/top-freq-buyers")
 def get_top_freq_buyers():
-
-    return render_template("ranking/ranking.html")
+    db = get_db()
+    cursor = db.cursor()
+    users = cursor.execute(
+        "SELECT u.id, u.firstname, u.lastname, b.author_id, COUNT(*)"
+        " FROM user u JOIN bid b ON u.id = b.author_id"
+        " GROUP BY u.id"
+        " ORDER BY COUNT(b.author_id) DESC"
+    )
+    return render_template("ranking/ranking.html", users=users)
 
 # Highest purchase
 @bp.route("/ranking/top-purchase")
 def get_top_purchase():
-    return render_template("ranking/ranking.html")
+    db = get_db()
 
-# Highest ask_price
-@bp.route("/ranking/top-ask-price")
+    posts = db.execute(
+        "SELECT p.id, title, description, image, price, duration, best_ask_price, p.status, p.created, p.author_id, username, u.firstname, u.lastname, p.disabledBid, p.image"
+        " FROM post p JOIN user u ON p.author_id = u.id AND p.best_ask_price NOT NULL"
+        " ORDER BY p.best_ask_price DESC"
+    ).fetchall()
+
+    return render_template("ranking/ranking.html", posts=posts)
+
+# Highest price
+@bp.route("/ranking/top-price")
 def get_top_ask_price():
     db = get_db()
 
     posts = db.execute(
         "SELECT p.id, title, description, image, price, duration, best_ask_price, p.status, p.created, p.author_id, username, u.firstname, u.lastname, p.disabledBid, p.image"
-        " FROM post p JOIN user u ON p.author_id = u.id"
-        " ORDER BY p.created DESC"
+        " FROM post p JOIN user u ON p.author_id = u.id AND p.price NOT NULL"
+        " ORDER BY p.price DESC"
     ).fetchall()
 
     return render_template("ranking/ranking.html", posts=posts)
