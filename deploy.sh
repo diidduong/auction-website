3c37bfd5de2b2752afeeab146ca85ee873596a89
Duy Duong <diidduong@gmail.com>
2022-12-11 20:21:29 -0700

    update

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 kubectl apply -f ./flaskr/flaskr-deployment.yaml
 kubectl apply -f flaskr/flaskr-ingress.yaml
 kubectl apply -f flaskr/flaskr-service.yaml
+
+kubectl port-forward --address 0.0.0.0 service/flaskr-service 5000:5000
